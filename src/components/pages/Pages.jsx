3aab02c1ffbb1e0f1dc6d8d695ca12f57a3441e7
Kashif Saeed <kashifsaeedman@gmail.com>
2025-03-12 16:29:32 +0500

    Implement Pricing section with responsive design and content

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -3,6 +3,7 @@ import Header from "../common/header/Header"
 import About from "../about/About";
 import Services from "../services/Services";
 import Blog from "../blog/Blog";
+import Pricing from "../pricing/Pricing";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "../home/Home"
 const Pages = () => {
@@ -15,6 +16,7 @@ const Pages = () => {
           <Route exact path="/about" component={About} />
           <Route exact path="/services" component={Services} />
           <Route exact path="/blog" component={Blog} />
+          <Route exact path="/pricing" component={Pricing} />
         </Switch>
       </Router>
     </>
