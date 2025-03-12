402232d7c9f235242edcda9a79ebe67af64fe51b
Kashif Saeed <kashifsaeedman@gmail.com>
2025-03-12 16:27:34 +0500

    Implement Blog section with responsive design and content

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "../common/header/Header"
 import About from "../about/About";
 import Services from "../services/Services";
+import Blog from "../blog/Blog";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "../home/Home"
 const Pages = () => {
@@ -13,6 +14,7 @@ const Pages = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/services" component={Services} />
+          <Route exact path="/blog" component={Blog} />
         </Switch>
       </Router>
     </>
